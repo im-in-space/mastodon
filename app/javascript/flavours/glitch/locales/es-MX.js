5f548be7309081d0bcdb31b9879566f7b84dd433
Lucy @ im-in.space <gh@kdy.ch>
2022-10-26 14:24:37 +0200

    Add/fix translations

--- app/javascript/flavours/glitch/locales/es-MX.js
+++ app/javascript/flavours/glitch/locales/es-MX.js
@@ -1,7 +1,11 @@
 import inherited from 'mastodon/locales/es-MX.json';
 
 const messages = {
-  //  No translations available.
+  'compose.attach.gif': 'Adjuntar un GIF',
+  'giphy.search': 'Buscar GIFs',
+  'giphy.error': '¡Uy! Algo está mal. Por favor, inténtelo más tarde.',
+  'giphy.loading': 'Cargando...',
+  'giphy.nomatches': 'No se han encontrado resultados.'
 };
 
 export default Object.assign({}, inherited, messages);
