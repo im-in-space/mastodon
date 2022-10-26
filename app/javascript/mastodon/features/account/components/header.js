ee6fdd783859ff94190a760d1f4613afd190a4ea
Lucy @ im-in.space <gh@kdy.ch>
2022-10-26 14:26:52 +0200

    Fix empty badges showing up

--- app/javascript/mastodon/features/account/components/header.js
+++ app/javascript/mastodon/features/account/components/header.js
@@ -277,7 +277,7 @@ class Header extends ImmutablePureComponent {
       badge = (<div className='account-role bot'><FormattedMessage id='account.badges.bot' defaultMessage='Bot' /></div>);
     } else if (account.get('group')) {
       badge = (<div className='account-role group'><FormattedMessage id='account.badges.group' defaultMessage='Group' /></div>);
-    } else if (account.get('role')) {
+    } else if (account.get('role') && account.get('role').get('name') != '') {
       badge = (<div className={`account-role user-role-${account.get('role').get('id')}`}>{account.get('role').get('name')}</div>);
     } else {
       badge = null;
